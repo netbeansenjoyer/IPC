3851824f8b90d8470792853ee7693eb35a41872c
Sblasco44 <132147607+Sblasco44@users.noreply.github.com>
2023-05-14 19:13:20 +0200

    Add files via upload

---
+++ TrabajoIPC/src/javafxmlapplication/SignUpController.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package javafxmlapplication;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author Sblasco
+ */
+public class SignUpController implements Initializable {
+
+    @FXML
+    private Button botA;
+    @FXML
+    private Button botC;
+    @FXML
+    private Button fotografia;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void aceptar(ActionEvent event) {
+    }
+
+    @FXML
+    private void cancelar(ActionEvent event) {
+    }
+
+    @FXML
+    private void selFoto(ActionEvent event) {
+    }
+    
+}
