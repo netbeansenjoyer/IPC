1d5f60e95b134044aa8d45d3725093d3e32cec13
netbeansenjoyer <132113447+netbeansenjoyer@users.noreply.github.com>
2023-05-23 10:30:33 +0200

    Add files via upload

---
+++ IPC-main-3/TrabajoIPC/src/javafxmlapplication/FXMLDocumentController.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxmlapplication;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.Modality;
+import javafx.scene.Scene;
+
+
+/**
+ *
+ * @author jsoler
+ */
+public class FXMLDocumentController implements Initializable {
+    private Label labelMessage;
+    @FXML
+    private AnchorPane Panel;
+    @FXML
+    private Button LogIn;
+    @FXML
+    private Button SignUp;
+    @FXML
+    private Button ReservarPista;
+    @FXML
+    private Button MisReservas;
+    @FXML
+    private Button PistasDisponibles;
+    @FXML
+    private Button Exit;
+    @FXML
+    private Button CalendarioReserva1;
+    @FXML
+    private Button hora1;
+    @FXML
+    private Button hora5;
+    @FXML
+    private Button hora6;
+    @FXML
+    private Button hora4;
+    @FXML
+    private Button hora3;
+    @FXML
+    private Button hora2;
+    
+    
+    //=========================================================
+    // event handler, fired when button is clicked or 
+    //                      when the button has the focus and enter is pressed
+    private void handleButtonAction(ActionEvent event) {
+        labelMessage.setText("Hello, this is your first JavaFX project - IPC");
+    }
+    
+    //=========================================================
+    // you must initialize here all related with the object 
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void hora1_action(ActionEvent event) {
+        
+    }
+
+    @FXML
+    private void Register(ActionEvent event) throws Exception{
+        FXMLLoader micargador = new FXMLLoader(getClass().getResource("SignUp.fxml"));
+        Parent root = micargador.load();
+        Stage stage = new Stage();
+       
+        SignUpController control = micargador.getController();
+        Scene scene= new Scene(root, 1000, 600);
+        stage.setScene(scene);
+        stage.setTitle("Sign Up");
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.showAndWait();
+    }
+
+    @FXML
+    private void LogIn(ActionEvent event) throws Exception {
+        FXMLLoader loader = new  FXMLLoader(getClass().getResource("LogIn.fxml"));
+        Parent root = loader.load();
+        Stage stage = new Stage();
+       
+        LogInController control = loader.getController();
+        Scene scene= new Scene(root, 1000, 600);
+        stage.setScene(scene);
+        stage.setTitle("Log In");
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.showAndWait();
+    }
+    
+}
---
+++ IPC-main-3/TrabajoIPC/src/javafxmlapplication/SignUpController.java
@@ -0,0 +1,247 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package javafxmlapplication;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import model.Club;
+import model.ClubDAOException;
+import model.Member;
+
+
+/**
+ * FXML Controller class
+ *
+ * @author Sblasco
+ */
+public class SignUpController implements Initializable {
+
+    @FXML
+    private Button botA;
+    @FXML
+    private Button botC;
+    @FXML
+    private Button fotografia;
+    
+    @FXML
+    private TextField TextNomb;
+    @FXML
+    private TextField TextApell;
+    @FXML
+    private TextField TextNick;
+    @FXML
+    private PasswordField TextPasw;
+    @FXML
+    private TextField TextTel;
+    @FXML
+    private TextField TextTar;
+    @FXML
+    private TextField TextSVC;
+    @FXML
+    private Label errName;
+    @FXML
+    private Label errApell;
+    @FXML
+    private Label errNick;
+    @FXML
+    private Label errpasw;
+    @FXML
+    private Label errTel;
+    @FXML
+    private Label errTar;
+    @FXML
+    private Label errsvc;
+    @FXML
+    private ImageView photo;
+    Image img;
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+      img = new Image("/avatars/default.png");
+      photo.setImage(img);
+      TextTar.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
+          if (!newValue.matches("\\d*")) {
+              TextTar.setText(newValue.replaceAll("[^\\d]", ""));
+          }
+          if(newValue.length() >16){TextTar.setText(oldValue);}
+      });
+      TextTel.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
+          if (!newValue.matches("\\d*")) {
+              TextTel.setText(newValue.replaceAll("[^\\d]", ""));
+          }
+          
+      });
+      TextSVC.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
+          
+          if (!newValue.matches("\\d*")) {
+              TextSVC.setText(newValue.replaceAll("[^\\d]", ""));
+          }
+          if(newValue.length() >3){TextSVC.setText(oldValue);}
+      });
+        TextNomb.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
+          if (!newValue.matches("[a-zA-Z]*")) {
+              TextNomb.setText(newValue.replaceAll("[^[a-zA-Z]]", ""));
+          }
+        });
+        TextApell.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
+          if (!newValue.matches("([a-zA-Z]*)")) {
+              TextApell.setText(newValue.replaceAll("[^([a-zA-Z]|[ \\t\\n\\x0B\\f\\r])]", ""));
+          }
+        });
+        TextNick.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
+          if (!newValue.matches("(([a-zA-Z]*)+\\d*)")) {
+              TextNick.setText(newValue.replaceAll("[^([a-zA-Z]|\\d)]", ""));
+          }
+        });
+}
+
+   @FXML
+    private void aceptar(ActionEvent event) throws ClubDAOException, IOException {
+        
+            
+        
+       
+        boolean aux = checkName(TextNomb.getText());
+        if(aux == false){
+           errName.setVisible(true);
+        }else{
+            errName.setVisible(false);
+        }
+        
+        boolean aux2 = checkApellidos(TextApell.getText());
+        if(aux2 == false){
+           errApell.setVisible(true);
+        }else{
+            errApell.setVisible(false);
+        }
+        
+        boolean aux3 = checkNick(TextNick.getText());
+        if(aux3 == false){
+           errNick.setVisible(true);
+        }else{
+            errNick.setVisible(false);
+        }
+        
+        boolean aux4 = checkPasw(TextPasw.getText());
+        if(aux4 == false){
+           errpasw.setVisible(true);
+        }else{
+            errpasw.setVisible(false);
+        }
+        boolean aux5 = checkTel(TextTel.getText());
+        if(aux5 == false){
+           errTel.setVisible(true);
+        }else{
+            errTel.setVisible(false);
+        }
+        
+        boolean aux6 = checkTar(TextTar.getText());
+        if(TextTar.getText().length() > 1){
+        if(aux6 == false){
+           errTar.setVisible(true);
+        }else{
+            errTar.setVisible(false);
+        }
+        }
+        boolean aux7 = checkSVC(TextSVC.getText());
+        int svc = 0;
+        if(TextSVC.getText().length() > 0){
+           svc = Integer.parseInt(TextSVC.getText());
+        if(aux7 == false){
+           errsvc.setVisible(true);
+        }else{
+            errsvc.setVisible(false);
+        }
+                        
+        }
+       
+        boolean aux8 = aux && aux2 && aux3 && aux4 && aux5;
+        if((aux8 && !aux6 && !aux7) || (aux8 && aux6 && aux7)){
+            Club club = Club.getInstance();
+            club.registerMember(TextNomb.getText(), TextApell.getText(), TextTel.getText(), TextNick.getText(), TextPasw.getText(), TextTar.getText(),svc,img);
+            botA.getScene().getWindow().hide();
+       
+        }
+    }
+
+    @FXML
+    private void cancelar(ActionEvent event) {
+      
+        botC.getScene().getWindow().hide();
+
+    }
+
+    @FXML
+    private void selFoto(ActionEvent event) throws FileNotFoundException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Abrir fichero");
+        fileChooser.getExtensionFilters().addAll(
+        
+        new ExtensionFilter("Imágenes", "*.png", "*.jpg"));
+        
+        File selectedFile = fileChooser.showOpenDialog(
+        ((Node)event.getSource()).getScene().getWindow());
+        if (selectedFile != null) {
+        img = new Image(new FileInputStream(selectedFile.getAbsolutePath()));
+        photo.setImage(img);
+        }
+    }
+    
+    private boolean checkName(String nombre){
+        return nombre.length() > 1;
+    }
+    
+    private boolean checkPasw(String nombre){
+        return nombre.length() >6;
+    }
+    private boolean checkNick(String nombre) throws ClubDAOException, IOException{
+        Club club = Club.getInstance();
+        return nombre.length() > 1 && !(club.existsLogin(nombre));
+        
+    }
+    private boolean checkTar(String nombre){
+        return nombre.length() > 15;
+    }
+    private boolean checkTel(String nombre) throws ClubDAOException, IOException{
+        return nombre.length() > 1;
+    }
+     private boolean checkSVC(String nombre){
+       return nombre.length() > 2;
+    }
+     private boolean checkApellidos(String nombre){
+        return nombre.length() > 1;
+    }
+
+}
